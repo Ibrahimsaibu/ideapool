9763addd5cc431d362d9069e053d4b53aabc4e3a
Ibrahim Saibu <ibrahimsaibu2010@gmail.com>
2022-08-29 05:30:41 -0700

    gitignore

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const connectDb = async ()=>{
+const connectDb = async () => {
    try {
-    await mongoose.connect(process.env.MONGO_URL as string)
-     console.log('mongo db connected')
+      await mongoose.connect(process.env.MONGO_URL as string)
+      console.log('mongo db connected')
    } catch (error) {
-    process.exit(1)
+      process.exit(1)
    }
 }
 
